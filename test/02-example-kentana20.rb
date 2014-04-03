871b404f613723a565c28d3a18c855eb4372ddc5
kentana20 <kentana@ks-book.local>
2014-04-03 17:44:02 +0900

    コケたテストを修正しました

--- test/02-example-kentana20.rb
+++ test/02-example-kentana20.rb
@@ -11,13 +11,13 @@ class HelloTest < Test::Unit::TestCase
   def test_kentana20
     get "/kentana20"
     assert_true(last_response.ok?)
-    assert_equal 'Hello, kentanya20!', last_response.body
+    assert_equal 'Hello, kentana20!', last_response.body
   end
 
   def test_kentana30
     get "/kentana30"
     assert_true(last_response.ok?)
-    assert_equal 'Hello, kentanya30!', last_response.body
+    assert_equal 'Hello, kentana30!', last_response.body
   end
 
 end
